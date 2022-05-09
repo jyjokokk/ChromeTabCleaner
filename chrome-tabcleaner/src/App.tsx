c23cbab5555cac3d0bc0610126e3a70424200951
Jyrki Kokkola <jyrki.j.kokkola@student.jyu.fi>
2022-05-09 09:17:39 +0300

    Main functionality (Had forgot to commit previously)

--- chrome-tabcleaner/src/App.tsx
+++ chrome-tabcleaner/src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Button from './components/Button'
 
-function App() {
+type Tab = chrome.tabs.Tab
+
+const App = (): JSX.Element => {
+  const [ tabs, setTabs ] = useState<Tab[]>([])
+
+  useEffect(() => {
+    chrome.tabs.query({ })
+      .then((resp) => {
+        setTabs(resp)
+      })
+  }, [])
+
+  const closeDuplicates = () => {
+    const tabList: Tab[] = [...tabs]
+    const seen = new Set();
+    const toClose: number[] = []
+    for (let tab of tabList) {
+      if (tab.id !== undefined && seen.has(tab.url)) {
+        toClose.push(tab.id)
+        continue
+      }
+      seen.add(tab.url)
+    }
+    chrome.tabs.remove(toClose)
+    setTabs([...new Set(tabList)])
+  }
+
+  const sortTabs = async () => {
+    const sorted = [...tabs]
+    sorted.sort((a, b) => {
+      return (a.url > b.url) ? 1
+           : (b.url > a.url) ? -1
+           : 0 }
+      )
+    for (let i = 0; i < sorted.length; i++) {
+      sorted[i].index = i;
+      await chrome.tabs.move(sorted[i].id, {"index": i})
+      alert(sorted[i].index)
+    }
+    setTabs(sorted);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button text="Close duplicates" handleClick={closeDuplicates} />
+      <Button text="Sort tabs" handleClick={sortTabs} />
     </div>
   );
 }
